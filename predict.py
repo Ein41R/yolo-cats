de360e0c2957559580e3ce2f33517c1b73cf9216
Einar Pfaff <einar.h.pfaff@gmail.com>
2026-04-11 12:00:14 +0200

    testing script

---
+++ predict.py
@@ -0,0 +1,23 @@
+# import cv2
+import os
+
+from ultralytics import YOLO
+
+model = YOLO("best.pt")
+
+dir = input("chooose imageset: (default=d/t/images), (1=d/t/images), (2=images/cat images)")
+
+if dir == "2":
+    images = os.listdir("images/cat images")
+    path = "images/cat images"
+
+else:
+    images = os.listdir("datasets/train/images")
+    path = "datasets/train/images"
+
+
+sample_size = input(f"how many images to predict (max: {len(images)})? (default=10)")
+
+for image in images[0:int(sample_size)]:
+    source = os.path.join(path, image)
+    model.predict(source, save=True, imgsz=320, conf=0.25)
--- run.py
+++ run.py
@@ -2,4 +2,15 @@ from ultralytics import YOLO
 
 model = YOLO("best.pt")
 
-results = model(source=0, show=True, conf=0.25, save=True)
+# results = model(source=0, show=True, conf=0.25, save=True)
+
+results = model("images/cat images/Image_208.jpg")  # predict on an image
+
+# Access the results
+for result in results:
+    xywh = result.boxes.xywh  # center-x, center-y, width, height
+    xywhn = result.boxes.xywhn  # normalized
+    xyxy = result.boxes.xyxy  # top-left-x, top-left-y, bottom-right-x, bottom-right-y
+    xyxyn = result.boxes.xyxyn  # normalized
+    names = [result.names[cls.item()] for cls in result.boxes.cls.int()]  # class name of each box
+    confs = result.boxes.conf  # confidence score of each box
