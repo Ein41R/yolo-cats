790108fa587d2979d7365541d7e5b67c6eebf453
Einar Pfaff <einar.h.pfaff@gmail.com>
2026-04-14 14:46:23 +0200

    fix: class issue in dataset

--- train.py
+++ train.py
@@ -1,5 +1,13 @@
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.yaml")
-
-model.train(data="config.yaml", epochs=500)#no improvement after 232 epochs
+model.train(
+    data="config.yaml",
+    epochs=300,
+    patience=50,
+    lr0=0.001,
+    optimizer="AdamW",
+    cos_lr=True,
+    close_mosaic=10,
+    imgsz=640,
+)
