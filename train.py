d1fdd0655a89373df6aba13bb598a70a0858dd9a
Einar Pfaff <einar.h.pfaff@gmail.com>
2026-04-14 14:12:48 +0200

    fixed issue

--- train.py
+++ train.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("yolov8n.yaml")
 
-model.train(data="config.yaml", epochs=500)//no improvement after 232 epochs
+model.train(data="config.yaml", epochs=500)#no improvement after 232 epochs
